b710af49048cf02bb4873cac434e41c272be3003
Dmytro Skaliun <dmytro.skaliun@capgemini.com>
2022-12-08 22:55:59 +0100

    added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <p>my app</p>
+    <div className='page'>
+      <Header />
+      <div className='container'>
+        <h1>SDM React App 2022</h1>
+      </div>
     </div>
   )
 }
